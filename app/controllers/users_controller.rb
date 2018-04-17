0bafdfd35be3fd3ca6ff1941271c58a1cd04a045
James Mahan <36861942+jcmahan@users.noreply.github.com>
2018-04-16 20:14:55 -0700

    "edit user functionality added"

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,6 +9,10 @@ class UsersController < ApplicationController
         puts params[:id]
     end
 
+    def edit
+        @user = User.find(params[:id])
+    end
+
     def create
         @user = User.new(user_params)
         if @user.save
